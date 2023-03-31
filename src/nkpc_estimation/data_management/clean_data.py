d2141348431bd306bbf5d8d37ce958f7c5465d4e
SvenEis <s6sveiss@uni-bonn.de>
2023-03-31 15:03:24 +0200

    Improved docstrings and overall structure of functions.

--- src/nkpc_estimation/data_management/clean_data.py
+++ src/nkpc_estimation/data_management/clean_data.py
@@ -1,6 +1,7 @@
 """Functions for cleaning the data sets."""
 
 import os
+import pathlib
 import zipfile
 from functools import reduce
 
@@ -8,82 +9,107 @@ import numpy as np
 import pandas as pd
 
 
-def load_data_files(data_files, dest_dir=None):
-    """Load data set.
+def load_data_files(
+    data_files: dict[str, str | pathlib.PosixPath],
+    dest_dir: str | None = None,
+) -> dict[str, pd.DataFrame]:
+    """Load data sets from csv and zip files.
 
     Args:
-        datafiles (dict): A dictionary with multiple paths for data sets.
-        dest_dir (optional, str): A path where unzipped data is stored. Default value is None.
+        data_files (Dict[str, Union[str, pathlib.PosixPath]]): A dictionary with file names and paths.
+        dest_dir (str, optional): The directory where zip files are extracted. Defaults to None.
+
+    Raises:
+        ValueError: If the input is not of the expected format.
+        FileNotFoundError: If a file is not found.
 
     Returns:
-        dictionary: The loaded pandas.DataFrame(s) stored in a dictionary.
+        Dict[str, pd.DataFrame]: The loaded pandas.DataFrame(s) stored in a dictionary.
 
     """
+    if not isinstance(data_files, dict):
+        raise ValueError("data_files must be a dictionary.")
+
     dataframes = {}
     for file_name, file_path in data_files.items():
-        file_extension = os.path.splitext(file_path)[1]
+        if not isinstance(file_name, str) or not isinstance(
+            file_path,
+            str | pathlib.PosixPath,
+        ):
+            raise ValueError(
+                "File names and paths must be strings or pathlib.PosixPath objects.",
+            )
+
+        file_path_str = str(file_path)
+        if not os.path.isfile(file_path_str):
+            raise FileNotFoundError(f"File not found: {file_path_str}")
+
+        file_extension = os.path.splitext(file_path_str)[1]
         if file_extension == ".csv":
-            header_num = 0
-            while True:
-                try:
-                    df = pd.read_csv(file_path, header=header_num)
-                    if df.shape[1] == 1:
-                        header_num += 1
-                    else:
-                        break
-                except:
-                    break
+            df = read_csv_file(file_path_str)
             dataframes[file_name] = df
         elif file_extension == ".zip":
-            assert (
-                dest_dir is not None
-            ), "dest_dir must be specified when reading a zip file."
-            zip_ref = zipfile.ZipFile(file_path, "r")
-            zip_ref.extractall(dest_dir)
-            for inner_file in zip_ref.namelist():
-                inner_file_path = os.path.join(dest_dir, inner_file)
-                if os.path.isfile(inner_file_path):
-                    inner_file_extension = os.path.splitext(inner_file_path)[1]
-                    if inner_file_extension == ".csv":
-                        header_num = 0
-                        while True:
-                            try:
-                                df = pd.read_csv(inner_file_path, header=header_num)
-                                if df.shape[1] == 1:
-                                    header_num += 1
-                                else:
-                                    break
-                            except:
-                                break
-                        dataframes[file_name] = df
-                    elif inner_file_extension == ".txt":
-                        continue
-                    else:
-                        raise AssertionError(
-                            "Unsupported file format in zip archive: Only .csv files are supported.",
-                        )
-            zip_ref.close()
+            if dest_dir is None:
+                raise ValueError("dest_dir must be specified when reading a zip file.")
+
+            with zipfile.ZipFile(file_path_str, "r") as zip_ref:
+                file_list = zip_ref.namelist()
+                if "Quarterly_Feb2023.csv" not in file_list:
+                    raise ValueError("File not found in zip archive.")
+                inner_file_path = os.path.join(dest_dir, "Quarterly_Feb2023.csv")
+                zip_ref.extract("Quarterly_Feb2023.csv", dest_dir)
+                df = read_csv_file(inner_file_path)
+                dataframes[file_name] = df
         else:
-            raise AssertionError(
-                "Unsupported file format: Only .csv, and .zip files are supported.",
+            raise ValueError(
+                "Unsupported file format: Only .csv and .zip files are supported.",
             )
+
     return dataframes
 
 
-def clean_data(data, data_info):
+def read_csv_file(file_path: str) -> pd.DataFrame:
+    """Read a csv file into a pandas DataFrame.
+
+    Args:
+        file_path (str): The path to the csv file.
+
+    Raises:
+        ValueError: If the csv file cannot be read.
+
+    Returns:
+        pd.DataFrame: The loaded DataFrame.
+
+    """
+    header_num = 0
+    while True:
+        try:
+            df = pd.read_csv(file_path, header=header_num)
+            if df.shape[1] == 1:
+                header_num += 1
+            else:
+                break
+        except Exception as e:
+            raise ValueError(f"Error reading csv file {file_path}: {str(e)}") from e
+    return df
+
+
+def clean_data(
+    data: dict[str, pd.DataFrame],
+    data_info: dict[str, any],
+) -> dict[str, pd.DataFrame]:
     """Clean data set. Information on data columns is stored in
     ``data_management/data_info.yaml``.
 
     Args:
-        data (pandas.DataFrame): The data set.
-        data_info (dict): Information on data set stored in data_info.yaml. The
-            following keys can be accessed:
-            - 'variables_to_keep': Names of columns that are keeped in data cleaning step
-            - 'column_rename_mapping': Old and new names of columns to be renamend,
-                stored in a dictionary with design: {'old_name': 'new_name'}
+        data: The data set.
+        data_info: Information on data set stored in data_info.yaml. The following keys can be accessed:
+            - 'variables_to_keep': Names of columns that are kept in data cleaning step
+            - 'dates_to_keep': Names of date columns to be kept
+            - 'column_rename_mapping': Old and new names of columns to be renamed, stored in a dictionary with design: {'old_name': 'new_name'}
 
     Returns:
-        pandas.DataFrame: The cleaned data sets in a dictionary.
+        The cleaned data sets in a dictionary.
 
     """
     combined_cols = data_info["variables_to_keep"].copy()
@@ -114,76 +140,116 @@ def clean_data(data, data_info):
     return df_dict
 
 
-def merge_data(dfs, index):
+def merge_data(
+    data: dict[str, pd.DataFrame],
+    index: str,
+    end_date: str = "2022-10-01",
+) -> pd.DataFrame:
     """Merge data sets.
 
     Args:
-        dfs (dict): A dictionary containing the dataframes to be merged.
+        data (Dict[str, pd.DataFrame]): A dictionary containing the dataframes to be merged.
         index (str): The column to use as the merge key.
+        end_date (str, optional): The latest date to include in the merged data set.
+            Defaults to "2022-10-01".
 
     Returns:
-        pandas.DataFrame: The merged data set.
+        pd.DataFrame: The merged data set.
+
+    Raises:
+        ValueError: If the input arguments are not of the expected type.
 
     """
-    dataframes = list(dfs.values())
+    # Validate input arguments
+    if not isinstance(data, dict):
+        raise ValueError("dataframes_dict must be a dictionary.")
+    if not isinstance(index, str):
+        raise ValueError("merge_key must be a string.")
+    if not isinstance(end_date, str):
+        raise ValueError("end_date must be a string.")
+
+    dataframes = list(data.values())
     df = reduce(lambda left, right: pd.merge(left, right, on=index), dataframes)
     df = df.set_index(index)
-    df = df[df.index <= "2022-10-01"]
+    df = df[df.index <= end_date]
     return df
 
 
-def calculateGrowthRate(data, variable):
-    """Calculate growth rates.
+def calculate_growth_rates(data: pd.DataFrame, variable: str) -> pd.DataFrame:
+    """Calculate growth rates for specified variables.
 
     Args:
         data (pandas.DataFrame): The data set.
-        variable (str): The variable name for which growth rates are calculated.
+        variable (str): The variable name for which growth rate is calculated.
 
     Returns:
-        pandas.DataFrame: The data set with growth rates instead of level variable.
+        pandas.DataFrame: The data set with growth rates for specified variables.
+
+    Raises:
+        ValueError: If the variable is not found in the `data` dataframe.
 
     """
+    if variable not in data.columns:
+        raise ValueError(f"Variable '{variable}' not found in the input dataframe.")
+
     growthRate = data[variable].pct_change(periods=1) * 100
-    data[variable] = growthRate
+    data[f"{variable}_growth_rate"] = growthRate
     data = data.dropna(axis=0)
     return data
 
 
-def calculateDeTrend(data, variable):
+def calculate_detrend(
+    data: pd.DataFrame,
+    variable: str,
+    order: int = 1,
+    detrended_column: str | None = None,
+) -> pd.DataFrame:
     """Detrend a time series data using linear regression.
 
     Args:
         data (pandas.DataFrame): The data set.
         variable (str): The variable which needs to be detrended.
+        order (int): The order of the polynomial used for the trend line.
+        detrended_column (str, optional): The name of the column to store the detrended data.
 
     Returns:
         pandas.DataFrame: The data set with the detrended variable.
 
+    Raises:
+        ValueError: If the input data has less than two data points.
+
     """
+    if len(data) < 2:
+        raise ValueError("Input data must have at least two data points")
+
     trend = np.arange(len(data[variable]))
-    coeffs = np.polyfit(trend, data[variable], 1)
+    coeffs = np.polyfit(trend, data[variable], order)
     trend = np.polyval(coeffs, trend)
     detrended = data[variable] - trend
-    data[variable] = detrended
+
+    if detrended_column is None:
+        detrended_column = f"{variable}_detrended"
+
+    data[detrended_column] = detrended
     return data
 
 
-def calculateExpectations(data, variable):
-    """Calculate backward looking expectations.
+def calculate_backward_expectations(
+    data: pd.DataFrame,
+    variable: str,
+    lag_period: int = 4,
+) -> pd.DataFrame:
+    """Calculate the backward-looking average for a given variable.
 
     Args:
-        data (pandas.DataFrame): The data set.
-        variable (str): The variable name for which expectations are calculated.
+        data (pd.DataFrame): The input data.
+        variable (str): The name of the variable for which expectations are calculated.
+        lag_period (int, optional): The number of periods to use for the backward-looking average. Default is 4.
 
     Returns:
-        pandas.DataFrame: The data set with backward looking expectations in a new column.
+        pd.DataFrame: The input data with a new column containing the backward-looking average.
 
     """
-    data["Backward_Expectations_" + variable] = 0.25 * (
-        data[variable].shift(1)
-        + data[variable].shift(2)
-        + data[variable].shift(3)
-        + data[variable].shift(4)
-    )
-    data = data.dropna(axis=0)
-    return data
+    expectations = data[variable].rolling(window=lag_period, min_periods=1).mean()
+    data[f"Backward_Expectations_{variable}"] = expectations
+    return data.dropna(axis=0)
--- src/nkpc_estimation/data_management/task_data_management.py
+++ src/nkpc_estimation/data_management/task_data_management.py
@@ -4,9 +4,9 @@ import pytask
 
 from nkpc_estimation.config import BLD, SRC
 from nkpc_estimation.data_management.clean_data import (
-    calculateDeTrend,
-    calculateExpectations,
-    calculateGrowthRate,
+    calculate_backward_expectations,
+    calculate_detrend,
+    calculate_growth_rates,
     clean_data,
     load_data_files,
     merge_data,
@@ -21,8 +21,8 @@ from nkpc_estimation.utilities import read_yaml
         "data": {
             "CPI": SRC / "data" / "DP_LIVE_29012023114147131.csv",
             "Emp": SRC / "data" / "DP_LIVE_29012023134946209.csv",
-            "NAIRU": SRC / "data" / "55022-2023-02-Historical-Economic-Data.zip",
             "MSC": SRC / "data" / "sca-table32-on-2023-Feb-12.csv",
+            "NAIRU": SRC / "data" / "55022-2023-02-Historical-Economic-Data.zip",
             "GDP": SRC / "data" / "GDP.csv",
             "Labor_share": SRC / "data" / "PRS85006173.csv",
         },
@@ -35,16 +35,25 @@ from nkpc_estimation.utilities import read_yaml
     },
 )
 def task_clean_data_python(depends_on, produces):
-    """Clean the data (Python version)."""
-    data_info = read_yaml(depends_on["data_info"])
+    """Clean the data.
+
+    Args:
+        depends_on (dict): Dependencies for the pytask function.
+        produces (dict): Path where the outcome is saved.
+
+    Returns:
+        csv: The cleaned data set as a csv file.
+
+    """
+    data_info = read_yaml(path=depends_on["data_info"])
     dfs = load_data_files(
         data_files=depends_on["data"],
         dest_dir=produces["unzipped_path"],
     )
-    dfs = clean_data(dfs, data_info)
-    dfs = merge_data(dfs=dfs, index="TIME")
-    dfs = calculateDeTrend(dfs, "GDP")
-    dfs = calculateGrowthRate(dfs, "CPI")
+    dfs = clean_data(data=dfs, data_info=data_info)
+    dfs = merge_data(data=dfs, index="TIME")
+    dfs = calculate_detrend(data=dfs, variable="GDP")
+    dfs = calculate_growth_rates(data=dfs, variable="CPI")
     dfs = dfs.rename(columns=data_info["column_rename_mapping"])
-    dfs = calculateExpectations(dfs, "Inflation")
+    dfs = calculate_backward_expectations(data=dfs, variable="Inflation")
     dfs.to_csv(produces["clean_data"], index=True)
