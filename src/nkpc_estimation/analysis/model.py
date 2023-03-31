9b511dc08957227cd27cf054a698f0531a8ac3ae
SvenEis <s6sveiss@uni-bonn.de>
2023-03-31 17:11:46 +0200

    Added docstring to function and print statements.

--- src/nkpc_estimation/analysis/model.py
+++ src/nkpc_estimation/analysis/model.py
@@ -1,38 +1,47 @@
 """Functions for fitting the regression model."""
 
+
+import pandas as pd
 import statsmodels.api as sm
 from statsmodels.iolib.smpickle import load_pickle
 from statsmodels.stats.diagnostic import acorr_breusch_godfrey, het_breuschpagan
 
 
-def fit_model(outcome_variable, feature_variables, model_type):
-    """Fit a model model to data.
+def fit_model(
+    outcome_variable: pd.Series,
+    feature_variables: list[pd.Series],
+    model_type: str,
+) -> sm.base.model.Results:
+    """Fit a model to data.
 
     Args:
-        data (pandas.DataFrame): The data set.
-        outcome_variable: The outcome variable of the regression.
-        feature_variables (list): A list of feature variables for the regression.
-        model_type (str): Type of regression.
+        outcome_variable (pandas.Series): The outcome variable of the regression.
+        feature_variables (List[pandas.Series]): A list of feature variables for the regression.
+        model_type (str): Type of regression. Only 'OLS' model_type is supported.
 
     Returns:
         statsmodels.base.model.Results: The fitted model.
 
     """
-    if model_type == "OLS":
-        model = sm.OLS(outcome_variable, feature_variables).fit()
-        BG = acorr_breusch_godfrey(model, nlags=4, store=False)
-        BP = het_breuschpagan(model.resid, feature_variables)
-        if BG[1] < 0.05 or BP[1] < 0.05:
-            model = sm.OLS(outcome_variable, feature_variables).fit(
-                cov_type="HAC",
-                cov_kwds={"maxlags": 4},
-            )
-        else:
-            model = sm.OLS(outcome_variable, feature_variables).fit()
-    else:
-        message = "Only 'OLS' model_type is supported."
-        raise ValueError(message)
+    if model_type != "OLS":
+        raise ValueError("Only 'OLS' model_type is supported.")
 
+    model = sm.OLS(outcome_variable, feature_variables).fit()
+    BG_pvalue = acorr_breusch_godfrey(model, nlags=4, store=False)[1]
+    BP_pvalue = het_breuschpagan(model.resid, feature_variables)[1]
+
+    if BG_pvalue < 0.05 or BP_value < 0.05:
+        model = sm.OLS(outcome_variable, feature_variables).fit(
+            cov_type="HAC",
+            cov_kwds={"maxlags": 4},
+        )
+        print(
+            f"Refitted model with HAC standard errors (BG p-value={BG_pvalue:.4f}, BP p-value={BP_pvalue:.4f}).",
+        )
+    else:
+        print(
+            f"Fitted model with OLS (BG p-value={BG_pvalue:.4f}, BP p-value={BP_pvalue:.4f}).",
+        )
     return model
 
 
