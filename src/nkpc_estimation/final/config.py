70cd3a3d89927cc48c1540380c65950b91930af2
SvenEis <s6sveiss@uni-bonn.de>
2023-03-31 11:15:19 +0200

    Added tables and plots for sensitivity analysis.

--- src/nkpc_estimation/final/config.py
+++ src/nkpc_estimation/final/config.py
@@ -1,6 +1,6 @@
 """Configuration of the plots."""
 
-from nkpc_estimation.analysis.config import _FEATURE_1, _FEATURE_2, _MODELS
+from nkpc_estimation.analysis.config import _DATES, _FEATURE_1, _FEATURE_2, _MODELS
 from nkpc_estimation.config import BLD
 
 PLOTS = {
@@ -14,6 +14,19 @@ PLOTS = {
     for feature_name_2 in _FEATURE_2
 }
 
+PLOTS_SENSITIVITY = {
+    f"{feature_name_1}_{feature_name_2}_{model_name}_{date}": {
+        "model": model_name,
+        "feature_1": feature_name_1,
+        "feature_2": feature_name_2,
+        "date": date,
+    }
+    for model_name in _MODELS
+    for feature_name_1 in _FEATURE_1
+    for feature_name_2 in _FEATURE_2
+    for date in _DATES
+}
+
 
 def path_to_plots(feature_name_1, feature_name_2, model_type):
     return (
@@ -21,6 +34,15 @@ def path_to_plots(feature_name_1, feature_name_2, model_type):
         / "python"
         / "figures"
         / f"{feature_name_1}_{feature_name_2}_{model_type}.pdf"
+    )
+
+
+def path_to_sensitivity_plots(feature_name_1, feature_name_2, model_type, date):
+    return (
+        BLD
+        / "python"
+        / "figures"
+        / f"{feature_name_1}_{feature_name_2}_{model_type}_{date}.pdf"
     )
 
 
@@ -35,6 +57,19 @@ TABLES = {
     for feature_name_2 in _FEATURE_2
 }
 
+TABLES_SENSITIVITY = {
+    f"{feature_name_1}_{feature_name_2}_{model_name}_{date}": {
+        "model": model_name,
+        "feature_1": feature_name_1,
+        "feature_2": feature_name_2,
+        "date": date,
+    }
+    for model_name in _MODELS
+    for feature_name_1 in _FEATURE_1
+    for feature_name_2 in _FEATURE_2
+    for date in _DATES
+}
+
 
 def path_to_tables(feature_name_1, feature_name_2, model_type):
     return (
@@ -42,4 +77,13 @@ def path_to_tables(feature_name_1, feature_name_2, model_type):
         / "python"
         / "tables"
         / f"{feature_name_1}_{feature_name_2}_{model_type}.tex"
+    )
+
+
+def path_to_sensitivity_tables(feature_name_1, feature_name_2, model_type, date):
+    return (
+        BLD
+        / "python"
+        / "tables"
+        / f"{feature_name_1}_{feature_name_2}_{model_type}_{date}.tex"
     )
--- src/nkpc_estimation/final/task_final.py
+++ src/nkpc_estimation/final/task_final.py
@@ -6,11 +6,21 @@ from statsmodels.iolib.summary2 import summary_col
 
 from nkpc_estimation.analysis.config import (
     path_to_estimation_result,
+    path_to_sensitivity_result,
 )
 from nkpc_estimation.analysis.model import load_model
 from nkpc_estimation.config import BLD
 from nkpc_estimation.final import plot_regression
-from nkpc_estimation.final.config import PLOTS, TABLES, path_to_plots, path_to_tables
+from nkpc_estimation.final.config import (
+    PLOTS,
+    PLOTS_SENSITIVITY,
+    TABLES,
+    TABLES_SENSITIVITY,
+    path_to_plots,
+    path_to_sensitivity_plots,
+    path_to_sensitivity_tables,
+    path_to_tables,
+)
 
 
 def _create_plot_parametrization(plots):
@@ -95,6 +105,88 @@ for id_, kwargs in _ID_TO_KWARGS.items():
                 )
 
 
+def _create_sensitivity_plot_parametrization(plots_sensitivity):
+    """Create pytask parametrization for plotting.
+
+    Args:
+        plots
+
+    Returns:
+        statsmodels.base.model.Results: The fitted model.
+
+    """
+    id_to_kwargs = {}
+    for name, config in plots_sensitivity.items():
+        depends_on = {
+            "data": BLD / "python" / "data" / "data_clean.csv",
+            "model": path_to_sensitivity_result(
+                config["feature_1"],
+                config["feature_2"],
+                config["model"],
+                config["date"],
+            ),
+        }
+        produces = path_to_sensitivity_plots(
+            config["feature_1"],
+            config["feature_2"],
+            config["model"],
+            config["date"],
+        )
+        id_to_kwargs[name] = {
+            "depends_on": depends_on,
+            "model": config["model"],
+            "produces": produces,
+        }
+
+    return id_to_kwargs
+
+
+_ID_TO_KWARGS_SENSITIVITY = _create_sensitivity_plot_parametrization(PLOTS_SENSITIVITY)
+
+for id_, kwargs in _ID_TO_KWARGS_SENSITIVITY.items():
+
+    @pytask.mark.task(id=id_, kwargs=kwargs)
+    def task_sensitivity_plot_regression(depends_on, model, produces):
+        data = pd.read_csv(depends_on["data"])
+        outcome_var = data["Inflation"]
+        feature_vars_1 = {
+            "Unemp": data["Unemployment"],
+            "Unemp_Gap": data["Unemployment"] - data["NAIRU"],
+            "Labor_share": data["Labor_share"],
+        }
+        feature_vars_2 = {
+            "BackExp": data["Backward_Expectations_Inflation"],
+            "MSC": data["MSC"],
+        }
+        yaxis_title = "Inflation (in %)"
+        x1axis_titles = {
+            "Unemp": "Unemployment Rate (in %)",
+            "Unemp_Gap": "Unemployment Gap (in %)",
+            "Labor_share": "NFB Labor Income Share (in %)",
+        }
+
+        x2axis_titles = {
+            "BackExp": "Backward Expectation (in %)",
+            "MSC": "MSC Expectation (in %)",
+        }
+
+        for feature_name_1, feature_var_1 in feature_vars_1.items():
+            for feature_name_2, feature_var_2 in feature_vars_2.items():
+                x1axis_title = x1axis_titles[feature_name_1]
+                x2axis_title = x2axis_titles[feature_name_2]
+                model = load_model(depends_on["model"])
+                fig = plot_regression(
+                    model,
+                    feature_var_1,
+                    feature_var_2,
+                    outcome_var,
+                    x1axis_title,
+                    x2axis_title,
+                    yaxis_title,
+                )
+                fig.write_image(produces)
+
+
 def _create_table_parametrization(tables):
     id_to_kwargs = {}
     for name, config in tables.items():
@@ -123,6 +215,55 @@ for id_, kwargs in _ID_TO_KWARGS.items():
 
     @pytask.mark.task(id=id_, kwargs=kwargs)
     def task_create_results_table_python(depends_on, model, produces):
+        """Create TeX tables of estimation results.
+
+        Args:
+            depends_on: Dependencies for the pytask function.
+            model:
+            produces: Path where the outcome is saved.
+
+        Returns:
+            latex table: A latex table with the regression results.
+
+        """
+        model = load_model(depends_on)
+        table = summary_col([model], stars=True, float_format="%0.2f").as_latex()
+        with open(produces, "w") as f:
+            f.writelines(table)
+
+
+def _create_table_sensitivity_parametrization(tables_sensitivity):
+    id_to_kwargs = {}
+    for name, config in tables_sensitivity.items():
+        depends_on = path_to_sensitivity_result(
+            config["feature_1"],
+            config["feature_2"],
+            config["model"],
+            config["date"],
+        )
+        produces = path_to_sensitivity_tables(
+            config["feature_1"],
+            config["feature_2"],
+            config["model"],
+            config["date"],
+        )
+        id_to_kwargs[name] = {
+            "depends_on": depends_on,
+            "model": config["model"],
+            "produces": produces,
+        }
+
+    return id_to_kwargs
+
+
+_ID_TO_KWARGS_SENSITIVITY = _create_table_sensitivity_parametrization(
+    TABLES_SENSITIVITY,
+)
+
+for id_, kwargs in _ID_TO_KWARGS_SENSITIVITY.items():
+
+    @pytask.mark.task(id=id_, kwargs=kwargs)
+    def task_create_results_sensitivity_table_python(depends_on, model, produces):
         """Create TeX tables of estimation results.
 
         Args:
